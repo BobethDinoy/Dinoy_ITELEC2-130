115696cc0b392a848a9fdc6391ea3d5ae394ae01
Beth <114622320+BobethDinoy@users.noreply.github.com>
2023-03-04 17:29:34 +0800

    Dinoy/24/02/2023: Linear, Relation, Constraint ACTIVITY

--- app/src/main/java/com/example/activitytoday/ListActivity1.java
+++ app/src/main/java/com/example/activitytoday/ListActivity1.java
@@ -3,14 +3,34 @@ package com.example.activitytoday;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class ListActivity1 extends AppCompatActivity {
+public class ListActivity1 extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list1);
-        Spinner mySpinner = (Spinner) findViewById(R.id.spinner2);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner2);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.numbers, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long l) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
